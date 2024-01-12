ee66621448a87cb604d3a13e06203c33917fb404
kimjaer <wofus0422@gmail.com>
2024-01-12 13:12:22 +0900

    location 상세주소 입력

--- theme/basic/location.php
+++ theme/basic/location.php
@@ -13,8 +13,19 @@ include_once(G5_THEME_PATH.'/head.php');
 
 >
 
-
+<div class="location_map">
+    <!-- <p class="font-size-30">찾아 오시는 길</p><br> -->
 <div id="map" style="width:100%;height:400px;"></div>
+<br>
+<br>
+<p>경기 광주시 경안로 8-2 4층<br>
+
+버거킹 옆 골목으로 들어오시면<br>
+다한차이나 건물(OK빌딩) 4층 입니다!<br>
+
+(902코인노래방 위 4층!)</p>
+</div>
+
 
 
 
